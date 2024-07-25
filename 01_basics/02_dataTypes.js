3106c0ffd76f925d038dbca4e1af93698f4a2173
Mayank Kalia <mayank2kalia@gmail.com>
2024-07-25 06:03:50 +0000

    datatypes

--- 01_basics/02_dataTypes.js
+++ 01_basics/02_dataTypes.js
@@ -1,5 +1,31 @@
 "use script"; //treat all JS code as newer version
 
-// alert("namaste"); // we are using nodejs, not brower
+// alert("namaste"); // we are using nodejs, not browser
 
-console.log(3)
+console.log(3 
+    + 
+    3) //code readability shold be high 
+
+console.log("Mayank")
+
+let name = "mayank"
+let age = 19
+let isLoggedIn = false
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value representation of empty value
+// undefined => 
+// symbol => unique (mostly we will discus this in "react" whenwe will need to differentiate between components)
+
+
+// object
+
+console.log(typeof undefined); //undefined
+console.log(typeof null); //object
+
+
+ 
+
