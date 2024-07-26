0408b1e65d106e76f04cbc922103e73d7f85f9e8
Mayank Kalia <mayank2kalia@gmail.com>
2024-07-26 11:02:35 +0000

    objects part 1

---
+++ 02_basics/03_objects.js
@@ -0,0 +1,14 @@
+// singleton
+//Object.create  =>this is one way of creating objects, issi ko contructor ke through bhi bola jata hai, isi ke andar "Singleton" banta hai
+
+//object literals  => this is anther way of creating objects
+const JsUser = {
+    name: "Mayank"
+    age: 18,
+    location: "India",
+    email: "user@google.com",
+    isLoggedIn: false,
+    lastLoginDays: ["Monday", "Saturday"]
+}
+
+cons
