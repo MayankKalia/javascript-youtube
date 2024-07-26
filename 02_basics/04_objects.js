d22332117cbb9b65622ac96661540d4b961bc6e5
Mayank Kalia <mayank2kalia@gmail.com>
2024-07-26 11:21:18 +0000

    object part 2

---
+++ 02_basics/04_objects.js
@@ -0,0 +1,10 @@
+//const tinderUser = new Object()
+const tinderUser = {}
+
+tinderUser.id = "123abc"
+tinderUser.name = "ankit"
+tinderUser.isLoggedIn = false
+console.log(tinderUser)
+
+
+console.log(tinderUser);
