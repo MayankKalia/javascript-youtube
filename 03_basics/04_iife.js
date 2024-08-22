ab0b8d41c5c3b230f209ec7a4732ccbd8938facc
Mayank Kalia <mayank2kalia@gmail.com>
2024-08-22 06:34:16 +0000

    iife

---
+++ 03_basics/04_iife.js
@@ -0,0 +1,16 @@
+// Immediately Invoked Function Expressions (IIFE)
+
+
+(function chai(){
+    // named IIFE
+    console.log(`DB CONNECTED`);
+})();
+
+( (name) => {
+    console.log(`DB CONNECTED TWO ${name}`);
+} )('hitesh')
+
+/* What is IIFE?
+   sirf yeh nahi bolna ki jo fxn immediately execute hota hai, INTERVIEW KE LIYE BOLNA HAI ki Global Scope ke pollution se problem hoti hai kai baar, toh uss Globl Scope ke jo variables hai yaan jo declarations hai toh uske pollution ko hatane ke liye humne IIFE ka use kiya hai */
+//1st parentheses jo hai vo hai fxn definition, second yala jo hai vo hai execution
+()()
