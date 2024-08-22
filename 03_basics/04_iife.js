6662c14ad16aa485a23b05d64c03936dc5c1294e
Mayank Kalia <mayank2kalia@gmail.com>
2024-08-22 07:34:21 +0000

    iife

--- 03_basics/04_iife.js
+++ 03_basics/04_iife.js
@@ -8,9 +8,9 @@
 
 ( (name) => {
     console.log(`DB CONNECTED TWO ${name}`);
-} )('hitesh')
+} )('mayank')
 
 /* What is IIFE?
    sirf yeh nahi bolna ki jo fxn immediately execute hota hai, INTERVIEW KE LIYE BOLNA HAI ki Global Scope ke pollution se problem hoti hai kai baar, toh uss Globl Scope ke jo variables hai yaan jo declarations hai toh uske pollution ko hatane ke liye humne IIFE ka use kiya hai */
-//1st parentheses jo hai vo hai fxn definition, second yala jo hai vo hai execution
+/* 1st parentheses jo hai vo hai fxn definition, second yala jo hai vo hai execution */
 ()()
